c305610f21e42cd4836f8e1e5f6b40e35ada94b3
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-10-19 16:21:58 +0900

    실습4-1

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-static int all_files;
+int counter;
 
-extern void sub();
-
+void set_counter()
+{
+	counter = 20;
+}
 int main(void)
 {
-	sub();
-	printf("%d\n", all_files);
+	printf("counter=%d\n", counter);
+	set_counter();
+	printf("counter=%d\n", counter);
 	return 0;
 }
